de4c0a5590c6541606bdd569b24af86abbe1c204
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-02-25 01:13:20 +0530

    mainhelper.py

--- mbot/utils/mainhelper.py
+++ mbot/utils/mainhelper.py
@@ -54,7 +54,7 @@ def thumb_down(link, name):
         else:
             file.write(
                 get(
-                    "https://telegra.ph/file/39bf16afe2fb5b0f13be3.jpg"
+                    "https://te.legra.ph/file/08f1061f32fc52b45a722.jpg"
                 ).content
             )
     return f"/tmp/thumbnails/{name}.jpg"
