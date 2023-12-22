d12328e5b2015ba05ed600addc161e72a47ab981
jaan op <125698743+ok-op@users.noreply.github.com>
2023-12-22 23:41:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ admins = {}
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5827766615").split()))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001719983565"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002126211125"))
 
 
 
